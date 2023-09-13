04a089c766c02f2a8b1be21117c82b104816fac2
bandiiskola <nandor.bere@gmail.com>
2023-09-13 11:02:28 +0200

    NincsKiiras

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,39 @@ function TagokFelvitele()
             tagok: []
         }
         lista.push(obj)
+        document.getElementById("diaknev").value=""
+        document.getElementById("diaklakohely").value=""
         document.getElementById("osztalyfelvitel").style.display="none"
         document.getElementById("tagfelvitel").style.display="block"
+        document.getElementById("diaknev").focus()
     }
+}
+
+function Mentes()
+{
+    if (document.getElementById("diaknev").value==""||document.getElementById("diaklakohely").value=="")
+    {
+        alert("Töltsd ki az adatokat!")
+    }
+
+    else
+    {
+        var obj={
+            diaknev: document.getElementById("diaknev").value,
+            diaklakohely: document.getElementById("diaklakohely").value,
+        }
+        lista[lista.length-1].tagok.push(obj)
+        document.getElementById("diaknev").value=""
+        document.getElementById("diaklakohely").value=""
+        document.getElementById("diaknev").focus()
+    }
+}
+
+function Kilep()
+{
+    document.getElementById("osztalynev").value=""
+    document.getElementById("osztalyfonoknev").value=""
+    document.getElementById("osztalyfelvitel").style.display="block"
+    document.getElementById("tagfelvitel").style.display="none"
+    document.getElementById("osztalynev").focus()
 }
